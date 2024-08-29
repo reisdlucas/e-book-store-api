63b2d1e984fb9fba0929b30f02aa8f030e359413
reisdlucas <reisdlucas@icloud.com>
2024-08-28 23:38:00 -0300

    :construction: (LivroRequest): Trabalho em processo

---
+++ src/main/java/com/book_store/e_book_store_api/api/http/resources/request/LivroRequest.java
@@ -0,0 +1,21 @@
+package com.book_store.e_book_store_api.api.http.resources.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LivroRequest {
+
+    @NotBlank(message = "Can't be blank")
+    @Schema(description = "Nome Livro")
+    private String nome;
+
+
+}
