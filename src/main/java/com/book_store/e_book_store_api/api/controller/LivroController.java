e0daa321393d4cd7d9984fbf057ff60cbf7996db
reisdlucas <reisdlucas@icloud.com>
2024-09-02 12:06:14 -0300

    :sparkles: (LivroController): Cria método para buscar todos os livros

--- src/main/java/com/book_store/e_book_store_api/api/controller/LivroController.java
+++ src/main/java/com/book_store/e_book_store_api/api/controller/LivroController.java
@@ -6,6 +6,7 @@ import com.book_store.e_book_store_api.core.mapper.LivroMapper;
 import com.book_store.e_book_store_api.domain.model.Livro;
 import com.book_store.e_book_store_api.domain.service.LivroService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -14,13 +15,12 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/livro")
@@ -40,7 +40,7 @@ public class LivroController {
     @PostMapping
     @Operation(description = "Cadastrar livro", summary = "Cadastrar livro")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Operação bem sucedida ",
+            @ApiResponse(responseCode = "201", description = "Operação bem sucedida",
             content = {@Content(mediaType = "application/json", schema = @Schema(implementation = LivroResponse.class))}),
             @ApiResponse(responseCode = "404", description = "Bad request")
     })
@@ -55,5 +55,22 @@ public class LivroController {
         LivroResponse livroResponse = livroMapper.mapEntityToResponse(createdLivro);
         return ResponseEntity.created(uri).body(livroResponse);
     }
+
+    @GetMapping
+    @ResponseBody
+    @Operation(description = "Buscar todos os livros", summary = "Buscar todos os livros")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operação bem sucedida",
+            content = @Content(array = @ArraySchema(schema = @Schema(implementation = LivroResponse.class)))),
+    })
+    public ResponseEntity<List<LivroResponse>> findAll() {
+        List<Livro> livro = livroService.findAll();
+        List<LivroResponse> livroResponse = livro.stream()
+                .map(livroMapper::mapEntityToResponse)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(livroResponse);
+    }
+
+
 
 }
