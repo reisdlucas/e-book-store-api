ec84f5d9d1dfeb81a0511ee2225d06ae291b06a4
reisdlucas <reisdlucas@icloud.com>
2024-09-06 09:56:18 -0300

    :sparkles: (AutorRequest): Cria AutorRequest

---
+++ src/main/java/com/book_store/e_book_store_api/api/http/resources/request/AutorRequest.java
@@ -0,0 +1,20 @@
+package com.book_store.e_book_store_api.api.http.resources.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AutorRequest {
+
+    @NotBlank(message = "Can't be blank")
+    @Schema(description = "Nome do autor")
+    private String name;
+
+}
